97e1215c4efe13de029c9282570608b4e5ecb10c
Mariana Carrillo <emecezeta25@gmail.com>
2026-04-18 20:40:43 +0000

    flask

--- src/app.py
+++ src/app.py
@@ -1,62 +1,22 @@
-from flask import Flask, request, render_template 
-import os
-from joblib import load
+from flask import Flask, request, render_template
+from pickle import load
 
 app = Flask(__name__)
-
-model =load(open("../models/salary_predictor_model.sav","rb")) 
-print("modelo cargado")
+model = load(open("/workspaces/Appwebs/models/salary_predictor_model.sav", "rb"))
 
 
-# Generar opciones dinámicas desde dataset
-#options = {
-  #  "job_title": sorted(df["job_title"].dropna().unique()),
-   # "education_level": sorted(df["education_level"].dropna().unique()),
-    #"industry": sorted(df["industry"].dropna().unique()),
-    #"company_size": sorted(df["company_size"].dropna().unique()),
-    #"location": sorted(df["location"].dropna().unique())
-#}
-
-def preprocess_input(data):
-    df_input = pd.DataFrame([data])
-
-    if columns is not None:
-        df_input = pd.get_dummies(df_input)
-
-        for col in columns:
-            if col not in df_input:
-                df_input[col] = 0
-
-        df_input = df_input[columns]
-
-    return df_input
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods = ["GET", "POST"])
 def index():
     prediction = None
-
     if request.method == "POST":
-        try:
-            input_data = {
-                "job_title": request.form.get("job_title"),
-                "experience_years": int(request.form.get("experience_years")),
-                "education_level": request.form.get("education_level"),
-                "skills_count": int(request.form.get("skills_count")),
-                "industry": request.form.get("industry"),
-                "company_size": request.form.get("company_size"),
-                "location": request.form.get("location"),
-                "remote_work": int(request.form.get("remote_work")),
-                "certifications": int(request.form.get("certifications"))
-            }
-
-            processed = preprocess_input(input_data)
-            prediction = round(model.predict(processed)[0], 2)
-
-        except Exception as e:
-            prediction = f"Error: {e}"
-
-    return render_template("index.html", prediction=prediction, options=options)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        
+        data = [[val1, val2, val3]]
+        salary = model.predict(data)[0]
+        rounded_salary = round(salary, -3)
+        prediction = f"${rounded_salary:,.0f} dólares"
+    
+    return render_template("index.html", prediction=prediction)
